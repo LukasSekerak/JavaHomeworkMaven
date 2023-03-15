86c680936c7dcbae2712f8731eb342658a4db5ea
LukasSekerak <lukas.sekerak@student.upjs.sk>
2023-03-15 14:19:50 +0100

    Initial commit

---
+++ academy/src/main/java/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(StringUtils.isEmpty("haha"));
+		//nájsť knižnicu na otvorenie obrázku a pridanie textu do neho
+		//v gitHube - poslať
+		//do piatka
+	}
+
+}
