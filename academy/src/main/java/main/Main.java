177f4c3736609cee2449123b3079d26bb5fedbf6
LukasSekerak <lukas.sekerak@student.upjs.sk>
2023-03-16 17:53:42 +0100

    Final commit

--- academy/src/main/java/main/Main.java
+++ academy/src/main/java/main/Main.java
@@ -2,29 +2,33 @@ package main;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.io.IOException;
 
 import ij.IJ;
 import ij.ImagePlus;
+import ij.io.FileSaver;
 import ij.process.ImageProcessor;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		/* 	Úloha nájsť knižnicu na otvorenie obrázku a pridanie textu do neho
 			v gitHube - poslať do piatka.
 		*/
 		
-		ImagePlus image = IJ.openImage("randomImage.jpg");
-		
+		ImagePlus image = IJ.openImage("randomimage.jpg");
 		Font font = new Font("Arial", Font.BOLD, 18);
 
 		ImageProcessor ip = image.getProcessor();
-		ip.setColor(Color.GREEN);
+		ip.setColor(Color.RED);
 		ip.setFont(font);
-		ip.drawString("This is a TEXT!", 0, 20);
+		ip.drawString("This is added TEXT!", 0, 20);
 		
-		image.show();
-
+		FileSaver f = new FileSaver(image);
+				
+		f.saveAsJpeg("newPicture.jpg");
+		
+		
 	}
 
 }
